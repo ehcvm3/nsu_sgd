3a8d88b96b599f735e77a9603495541e1c6cf7b5
Arthur Shaw <jashaw2002@gmail.com>
2025-02-25 12:32:07 -0500

    Ajouter un peu de documentation sur la nature du text à fournir sur le qnr

--- _details_serveur.R
+++ _details_serveur.R
@@ -6,4 +6,13 @@ serveur         <- ""
 espace_travail  <- ""
 utilisateur     <- ""
 mot_de_passe    <- ""
+
+# =============================================================================
+# Questionnaire sur Headquarters dont les données sont à passer en revue
+# =============================================================================
+
+# fournir un texte qui identifie le(s) questionanire. il peut s'agir du:
+# - nom complet
+# - sous-texte
+# - expression régulière
 nsu_qnr_expr    <- ""
